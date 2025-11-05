1f1bd197a38fdcbb02da75e71678dc962132fd55
KIRAN RAJ K <154965287+Kiranraj077@users.noreply.github.com>
2025-11-05 17:40:50 +0530

    Implement addEdge and printAns functions
    Added functions to add edges and print BFS result.

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -28,4 +28,30 @@ class Solution {
         }
         return bfs; 
     }
+void addEdge(vector <int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printAns(vector <int> &ans) {
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+}
+
+int main() 
+{
+    vector <int> adj[6];
+    
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 0, 4);
+
+    Solution obj;
+    vector <int> ans = obj.bfsOfGraph(5, adj);
+    printAns(ans);
+
+    return 0;
+}
 };
