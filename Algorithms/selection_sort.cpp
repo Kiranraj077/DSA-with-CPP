a16efb0cd38c1454896f1def521b709904660ba6
KIRAN RAJ K <154965287+Kiranraj077@users.noreply.github.com>
2025-11-10 17:07:09 +0530

    Create selection_sort.cpp

---
+++ Algorithms/selection_sort.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        // Find the index of the smallest element in the unsorted part
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the smallest element with the first element of the unsorted part
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
